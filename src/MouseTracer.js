c924166e5c2a09085b353bed8f6b9f8052848fe0
Norihiro Sakamoto <nori0620@gmail.com>
2011-05-07 15:11:33 +0900

    add sample

--- src/MouseTracer.js
+++ src/MouseTracer.js
@@ -12,28 +12,13 @@ var ns = com.nori0620.mouse_tracer;
         }
     };
     var HtmlHeplers = {
-        renderController : function(){
-            var controller = document.createElement('div');
-            controller.appendChild( document.createTextNode('StopAndReplay') );
-            controller.style.background = "#FFF";
-            controller.style.border     = "3px solid #F00";
-            controller.style.position = 'absolute';
-            controller.style.top   = 0;
-            controller.style.right = 0;
-            controller.style.zIndex = 9999999;
-            controller.addEventListener('click',function(){
-                ns.replay();
-                controller.style.display = 'none';
-            },true);
-            document.body.appendChild(controller);
-        },
-        renderCanvas : function(){
+        overlayCanvas : function(){
             var canvas = document.createElement('canvas');
             canvas.style.width  = document.body.clientWidth  + "px";
             canvas.style.height = document.body.clientHeight + "px";
             canvas.width  = document.body.clientWidth;
             canvas.height = document.body.clientHeight;
-            canvas.style.zIndex = 9999999;
+            canvas.style.zIndex = 999999;
             canvas.style.position = 'absolute';
             canvas.style.top  = 0;
             canvas.style.left = 0;
@@ -68,20 +53,24 @@ var ns = com.nori0620.mouse_tracer;
     /* --- Replayer ---- */
     var Replayer = function(history){
         this.history  = history;
-        this.canvas   = null;
         this.FRAME_INTERVAL = 10;
         this.current_frame  = 1;
+        this.canvas      = null;
+        this.interval_id = null;
+        this.callback    = null;
+        this.render_record_count = 0;
     };
     (function(proto){
-        proto.play = function(canvas){
+        proto.play = function(canvas, callback){
             if(!canvas.getContext){
                 throw "canvas is invalid";
             }
             this.canvas = canvas;
+            this.callback = callback;
             var that = this;
-            window.setInterval(
+            this.interval_id = window.setInterval(
                 function(){ that.render_frame(); } ,
-                this.FRAME_INTERVAL
+                that.FRAME_INTERVAL
             );
         };
         proto.render_frame = function(){
@@ -89,34 +78,82 @@ var ns = com.nori0620.mouse_tracer;
             var records = this.history.records;
             var from = ( this.current_frame -1 )*this.FRAME_INTERVAL;
             var to   = this.current_frame*this.FRAME_INTERVAL;
+            var renderer = new Renderer(ctx);
 
             var record,i;
             for( i=from; i < to; i++){ 
                 record = records[i];
                 if( record ){
-                    ctx.fillRect(record.x,record.y,3,3);
+                    renderer.render( record );
+                }
+                this.render_record_count++;
+                //console.log( 'render', 'total:',this.render_record_count,'length:',records.length);
+                if( this.render_record_count > records.length ){
+                    window.clearInterval( this.interval_id );
+                    if( this.callback ){ this.callback() };
+                    break;
                 }
             }
             this.current_frame++;
         };
     }(Replayer.prototype));
 
+    /* --- Renderer ---- */
+    var Renderer = function(context){
+        this.context = context;
+    };
+    (function(proto){
+        proto.render = function(record){
+            if( record.type == 'mousemove' ){
+                this._render_mousemove(record);
+            }
+            else if( record.type == 'click' ){
+                this._render_click(record);
+            }
+        };
+        proto._render_mousemove = function(record){
+            this.context.beginPath();
+            this.context.strokeStyle = "rgba(0,0,255,1)";
+            this.context.fillStyle   = "rgba(0,0,255,0.2)";
+            this.context.arc(record.x , record.y, 5, Math.PI*2, false);
+            this.context.stroke();
+            this.context.fill();
+        }
+        proto._render_click = function(record){
+            this.context.beginPath();
+            this.context.strokeStyle = "rgba(0,0,255,1)";
+            this.context.fillStyle   = "rgba(0,0,255,0.2)";
+            this.context.arc(record.x , record.y, 30, Math.PI*2, false);
+            this.context.stroke();
+            this.context.fill();
+        }
+    }(Renderer.prototype));
+
     /* --- Export --------- */
     (function(){
-        var history = new History( Utils.current_time() );
+        var history;
         var recorder = function(e){ history.addRecord(e); };
-        ns.record = function(){
-            HtmlHeplers.renderController();
+        var canvas;
+        /* Main API */
+        ns.record = function(callback){
+            history  = new History( Utils.current_time() );
             document.body.addEventListener("mousemove",recorder, true );
             document.body.addEventListener("click", recorder, true);
+            if( canvas ){ canvas.width = canvas.width + 1; canvas.display = "none"; }
+            if( callback ){ callback(); }
         };
-        ns.replay = function(){
-            var canvas = HtmlHeplers.renderCanvas();
+        ns.stop = function(callback){
             document.body.removeEventListener("mousemove", recorder, true);
             document.body.removeEventListener("click", recorder, true);
+            if( callback ){ callback(); }
+            return history;
+        }
+        ns.replay = function(callback){
+            canvas = HtmlHeplers.overlayCanvas();
             var replayer = new Replayer(history);
-            replayer.play( canvas );
+            replayer.play( canvas, callback );
         };
+        /* Control Panel */
     }());
 
 }());
